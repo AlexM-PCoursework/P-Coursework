1857ef69789e44aba98072cdc3f1a62ec0b2988b
alexmatt101 <matthewsag@dulwich.org.uk>
2018-08-17 20:16:02 +0100

    Added key control for left and right keys,
    syntax error, USED "." INSTEAD OF "_" line 21

--- Coursework 2.py
+++ Coursework 2.py
@@ -12,14 +12,24 @@ BLUE = (0,0,255)
 WIDTH = 1024
 HEIGHT = 500
 
+
 #player sprite
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50,50))
         self.image.fill(BLUE)
-        self.rect = self.image.get.rect()
+        self.rect = self.image.get_rect()
         self.rect.center = (25,HEIGHT - 25)
+
+    def update(self):
+       self.speedx = 0
+       keystate = pygame.key.get_pressed()
+       if keystate[pygame.K_LEFT]:
+            self.speedx = -5
+       if keystate[pygame.K_RIGHT]:
+            self.speedx= 5
+       self.rect.x = self.speedx
 
 # Open a new window
 size = (1024, 500)
