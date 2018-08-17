b57eba9dddac2137d1a4dd4dc0b1fc373f42ebcd
alexmatt101 <matthewsag@dulwich.org.uk>
2018-08-17 18:28:16 +0100

    Initialise Colours

---
+++ Coursework 2.py
@@ -0,0 +1,13 @@
+# Import the pygame library and initialise the game engine
+import pygame
+pygame.init()
+
+# Define some colors (for now)
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+GREEN = ( 0, 255, 0)
+RED = ( 255, 0, 0)
+BLUE = (0,0,255)
+
+WIDTH = 1024
+HEIGHT = 500
